079a425baa0437f3c9c1bd3b4ad0b253ed76bd6e
javaclass <javaclass@15000504cpu.fec.org>
2017-09-26 15:23:35 -0500

    Calculator method

---
+++ src/basics1/Calculator.java
@@ -0,0 +1,18 @@
+package basics1;
+
+public class Calculator{
+	public static void main(String[] args) {
+		System.out.println(calculateSum(5, 10));
+		
+	}
+	
+	static int calculateSum(int a, int b) {
+		return a+b; 	
+		}
+
+	
+}
+
+
+
+
