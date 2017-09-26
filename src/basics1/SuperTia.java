2c7eebb55bac0dac29761e579a10a9fe33d38ba8
javaclass <javaclass@15000504cpu.fec.org>
2017-09-26 10:18:28 -0500

    Song by Destiny's Child- SayMyName

---
+++ src/basics1/SuperTia.java
@@ -0,0 +1,10 @@
+package basics1;
+
+public class SuperTia {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+System.out.println("Say myname say my name, when no one is around you say baby, I lov you say my name say my name, you actring kind of crzy aint calling me baby, better say my name######Every other day, I woud call you would say babay hows your day, but today it aintthe same");
+	}
+
+}
