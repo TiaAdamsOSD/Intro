2c68d1917797837ca05a28f778ba7411f06974d2
javaclass <javaclass@15000504cpu.fec.org>
2017-09-27 14:53:45 -0500

    Looping my name

---
+++ src/basics1/NameLoop.java
@@ -0,0 +1,14 @@
+package basics1;
+
+public class NameLoop {
+
+	public static void main(String[] args) {
+		int nameLoop=1;
+		 while(nameLoop<=10){
+	System.out.println("Tia");
+	nameLoop = nameLoop +1;
+	}
+
+	}
+
+}
