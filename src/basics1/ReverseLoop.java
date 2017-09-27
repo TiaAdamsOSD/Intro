d19c010c59d9cbc6cadc3d981a54f44b680f7c4c
javaclass <javaclass@15000504cpu.fec.org>
2017-09-27 15:25:03 -0500

    Reverse Loop

---
+++ src/basics1/ReverseLoop.java
@@ -0,0 +1,14 @@
+package basics1;
+
+public class ReverseLoop {
+
+	public static void main(String[] args) {
+		int numberLoopReverse=100;
+		 while(numberLoopReverse > 43){
+	System.out.println(numberLoopReverse);
+	numberLoopReverse = numberLoopReverse -1;
+	}
+
+	}
+
+}
