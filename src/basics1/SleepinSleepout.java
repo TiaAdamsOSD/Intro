8c397502f96da0b7707755cbd47b23524fc83bb1
javaclass <javaclass@15000504cpu.fec.org>
2017-10-02 11:22:41 -0500

    Check if Sleep
    the parameter weekday is true if its is a weekday, and the parameter
    vacation is true if we are on vacation. we sleep in if it is not a
    weekday or we're on vacation. return true if we seep in

---
+++ src/basics1/SleepinSleepout.java
@@ -0,0 +1,16 @@
+package basics1;
+
+public class SleepinSleepout {
+
+	public static void main(String[] args) {
+		System.out.println(checkIShouldSleepIn(true,false));
+	}
+	static boolean checkIShouldSleepIn(boolean weekday, boolean vacation) {
+		
+		if(!weekday || vacation) {
+			return true;
+		}
+		else
+			return false;
+	}
+}
